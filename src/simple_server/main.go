f45cae45f07d2f085cc7cd4f4696c66e49a70291
Jasti Sri Radhe Shyam <samabhasatejsrs@outlook.com>
2019-12-03 22:28:20 +0530

    add simple server

---
+++ src/simple_server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
